a83c076743115d50adb534a2bfea4c3580c6f68a
Thuy Quematon <115153368+Thuggonaut@users.noreply.github.com>
2024-05-08 15:57:52 +0930

    Update megaphone.cpp

--- Cpp00/ex00/megaphone.cpp
+++ Cpp00/ex00/megaphone.cpp
@@ -3,7 +3,18 @@
 #include <cctype> //Test/manipulate characters
 
 //Read Command-line Arguments
+int	main(int argc, char **argv)
+{
 	//Check Argument Count: at least one argument is provided. If not, print message and exit
-	//Concatenate Arguments into a single string
-	//Convert to Uppercase
-	//Print to the console
+	if (argc > 1)
+	{
+		//Concatenate Arguments into a single string
+		
+		
+		//Convert to Uppercase
+		//Print to the console
+	}
+	else
+		std::cout << "Error: Please enter message after <./megaphone>" std::endl;
+	return (0);
+}
