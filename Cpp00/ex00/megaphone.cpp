07aa3c7c80d9483132ba881fab2791755fb88cab
Thuy Quematon <thuytkq@outlook.com>
2024-05-09 18:54:34 +0930

    u

--- Cpp00/ex00/megaphone.cpp
+++ Cpp00/ex00/megaphone.cpp
@@ -1,24 +1,41 @@
-#include <iostream>
-#include <cstring> //String manipulation
-#include <cctype> //Test/manipulate characters
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tquemato <tquemato@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 16:29:30 by tquemato          #+#    #+#             */
+/*   Updated: 2024/05/09 18:43:38 by tquemato         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-//Read Command-line Arguments
+#include <iostream>
+#include <cstring> //String manipulation like strcat()
+#include <cctype> //Test/manipulate characters like toupper()
+
+//Check Argument Count: at least one argument is provided. If not, print message and exit
+//Declare a character array to store the concatenated arguments, initialized with 0's
+//Concatenate Arguments into a single string using strcat()
+//Convert to Uppercase
+//Print to the console
+//Note the for loops: `i` is incremented after the first strcat() execution and updated before subsequent iterations
+
 int	main(int argc, char **argv)
 {
-	//Check Argument Count: at least one argument is provided. If not, print message and exit
 	if (argc >= 2)
 	{
-		char *res;
+		char res[1000] = {0};
 
-		while (argc >= 2)
-		{
-			//Concatenate Arguments into a single string
-			//Doesn't print double quotes: a string can be enclosed in quotes or without
-			//Convert to Uppercase
-			//Print to the console
-		}
+		for (int i = 1; i < argc; ++i)
+			strcat(res, argv[i]);
+
+		for (int i = 0; res[i] != '\0'; ++i)
+			res[i] = std::toupper(res[i]);
+		
+		std::cout << res << std::endl;
 	}
 	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	return (0);
 }
---
+++ Cpp00/wb.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+int main()
+{
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return 0;
+}
