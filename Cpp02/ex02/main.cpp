9440903719e8d13871a118225ec4b5ee24d60f8d
Thuy Quematon <thuytkq@outlook.com>
2024-07-26 21:45:26 +0930

    u

--- Cpp02/ex02/main.cpp
+++ Cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tquemato <tquemato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 14:36:14 by tquemato          #+#    #+#             */
-/*   Updated: 2024/07/13 00:34:07 by tquemato         ###   ########.fr       */
+/*   Updated: 2024/07/26 21:45:19 by tquemato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int main( void ) 
 {
 	Fixed a; //Call default constructor
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) ); //Call copy constructor, then call constructor from float multiplied by constructor from int, store result in fpValue
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 )); //Call copy constructor, then call constructor from float multiplied by constructor from int, store result in fpValue
 
 	std::cout << a << std::endl; //Print the default value of a
 	std::cout << ++a << std::endl; //Increment a and print the new value
@@ -28,3 +28,28 @@ int main( void )
 
 	return 0;
 }
+
+/*
+Default constructor called
+Int constructor called
+Float constructor called
+Float constructor called
+Destructor called			- for Fixed(5.05)
+Destructor called			- for Fixed(2)
+0
+Copy constructor called
+0.00390625
+Destructor called			- operator<< overload ++a
+0.00390625
+Copy constructor called
+0.00390625
+Destructor called			- operator<< overload a++
+0.0078125
+Copy constructor called
+10.1016
+Copy constructor called
+10.1016
+Destructor called			- for max(a, b)
+Destructor called			- for b
+Destructor called			- for a
+*/
